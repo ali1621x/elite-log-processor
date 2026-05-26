f61e568346d10bb906940d0139941b1ebe86caba
10$99x00 <aliayran2221@gmail.com>
2026-05-27 02:55:13 +0300

    Create ulp_database.dart

---
+++ lib/ulp_database.dart
@@ -0,0 +1,17 @@
+class UlpDatabase {
+  static final Map<String, List<String>> categories = {
+    'netflix': ['netflix.com', 'nflx'],
+    'spotify': ['spotify.com', 'sptfy'],
+    'paypal': ['paypal.com', 'paypal'],
+    'steam': ['steampowered.com', 'steam'],
+    'amazon': ['amazon.com', 'amzn'],
+    'gmail': ['gmail.com', 'google.com'],
+    'discord': ['discord.com', 'discordapp'],
+    'facebook': ['facebook.com', 'fb.com'],
+    'instagram': ['instagram.com'],
+    'twitter': ['twitter.com', 'x.com'],
+    'tiktok': ['tiktok.com'],
+    'chatgpt': ['openai.com', 'chatgpt'],
+    'banking': ['bank', 'chase', 'wellsfargo', 'bofa'],
+  };
+}
